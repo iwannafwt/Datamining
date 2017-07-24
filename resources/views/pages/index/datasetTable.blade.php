da2ba419ec04f4b87953ce92e9c56745041b61de
iwannafwt <iwanna.fwtiadoy@gmail.com>
2017-07-24 19:08:30 +0300

    Add dataset table

---
+++ resources/views/pages/index/datasetTable.blade.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <P>HI</P>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
